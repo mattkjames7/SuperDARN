0569b58553b21a6426ecee1808e162d63338a606
Matt <mattkjames7@gmail.com>
2022-04-04 20:21:53 +0100

    added setup.py

--- SuperDARN/__init__.py
+++ SuperDARN/__init__.py
@@ -1,3 +1,6 @@
+__version__ = '0.1.0'
+
+
 from . import Globals
 from . import Hardware
 from . import Data
---
+++ setup.py
@@ -0,0 +1,60 @@
+import setuptools
+from setuptools.command.install import install
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def getversion():
+	'''
+	read the version string from __init__
+	
+	'''
+	#get the init file path
+	thispath = os.path.abspath(os.path.dirname(__file__))+'/'
+	initfile = thispath + 'SuperDARN/__init__.py'
+	
+	#read the file in
+	f = open(initfile,'r')
+	lines = f.readlines()
+	f.close()
+	
+	#search for the version
+	version = 'unknown'
+	for l in lines:
+		if '__version__' in l:
+			s = l.split('=')
+			version = s[-1].strip().strip('"').strip("'")
+			break
+	return version
+	
+version = getversion()
+
+setuptools.setup(
+    name="SuperDARN",
+	version=version,
+    author="Matt James",
+    author_email="mkj13@leicester.ac.uk",
+    description="Tools for reading and plotting SuperDARN data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mattkjames7/SuperDARN",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+    ],
+    install_requires=[
+		'numpy',
+		'scipy',
+		'matplotlib',
+		'DateTimeTools',
+		'aacgmv2',
+		'PyFileIO',
+	],
+	include_package_data=True,
+)
+
+
+
